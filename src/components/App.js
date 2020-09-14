0749c732025d5f03746803e88c1829486e6642df
giannisclipper <strunstrun@gmail.com>
2020-09-14 19:30:28 +0300

    Updating the header of the app (`GotoDates`, `GotoSettings`).

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import '../styles/libs/initialize.css';
 import '../styles/App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCompass } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faCog, faCompass } from '@fortawesome/free-solid-svg-icons';
 import { STATEContextProvider } from './STATEContext';
 import { REFContextProvider } from './REFContext';
 import { REFContext } from './REFContext';
@@ -16,6 +16,8 @@ function App() {
         <div className="App">
 
             <div className="title">
+                <GotoDates />
+                <GotoSettings />
                 <span>Diaries by GiannisClipper</span>
                 <ScrollToCentralDate />
             </div>
@@ -28,10 +30,22 @@ function App() {
     );
 }
 
+const GotoDates = () => {
+    return (
+        <a href="/dates"> 
+            <FontAwesomeIcon icon={ faHome } className="icon" title="Ημερολόγο" />
+        </a>
+    )
+}
+const GotoSettings = () => {
+    return (
+        <a href="/settings"> 
+            <FontAwesomeIcon icon={ faCog } className="icon" title="Ρυθμίσεις" />
+        </a>
+    )
+}
 const ScrollToCentralDate = () => {
-
     const REF = useContext( REFContext );
-
     return (
         <button onClick={ event => REF.current.scrollToCentralDate( event )}>
             <FontAwesomeIcon icon={ faCompass } className="icon" title="Μετακίνηση στην κεντρική ημ/νία" />
