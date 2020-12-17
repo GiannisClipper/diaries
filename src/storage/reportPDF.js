c4c844cefcba77d6b1ff19f45041de8f4c36f95b
giannisclipper <strunstrun@gmail.com>
2020-12-17 10:58:12 +0200

    Updating greek fonts in `jsPDF`.

--- src/storage/reportPDF.js
+++ src/storage/reportPDF.js
@@ -1,7 +1,39 @@
-import { jsPDF } from "jspdf";
-import { addFonts } from './fonts/jsPDFGreekFonts';
+/*
+jsPDF: Use of Unicode Characters / UTF-8 ( https://bestofjs.org/projects/jspdf )
 
-jsPDF.API.events.push( [ 'addFonts', addFonts ] );
+The 14 standard fonts in PDF are limited to the ASCII-codepage. If you want to use UTF-8 you have to integrate a custom font, which provides the needed glyphs. jsPDF supports .ttf-files. So if you want to have for example Chinese text in your pdf, your font has to have the necessary Chinese glyphs. So, check if your font supports the wanted glyphs or else it will show garbled characters instead of the right text.
+
+To add the font to jsPDF use our fontconverter in /fontconverter/fontconverter.html. The fontconverter will create a js-file with the content of the provided ttf-file as base64 encoded string and additional code for jsPDF. You just have to add this generated js-File to your project. You are then ready to go to use setFont-method in your code and write your UTF-8 encoded text.
+
+Alternatively you can just load the content of the *.ttf file as a binary string using fetch or XMLHttpRequest and add the font to the PDF file:
+const doc = new jsPDF();
+const myFont = ... // load the *.ttf font file as binary string
+// add the font to jsPDF
+doc.addFileToVFS("MyFont.ttf", myFont);
+doc.addFont("MyFont.ttf", "MyFont", "normal");
+*/
+
+import { jsPDF } from "jspdf";
+
+import addCourierNewNormal from './fonts/courierNewNormal';
+import addCourierNewBold from './fonts/courierNewBold';
+import addCourierNewItalic from './fonts/courierNewItalic';
+import addCourierNewBoldItalic from './fonts/courierNewBoldItalic';
+
+import addTrebuchetMSNormal from './fonts/trebuchetMSNormal';
+import addTrebuchetMSBold from './fonts/trebuchetMSBold';
+import addTrebuchetMSItalic from './fonts/trebuchetMSItalic';
+import addTrebuchetMSBoldItalic from './fonts/trebuchetMSBoldItalic';
+
+jsPDF.API.events.push( [ 'addFonts', addCourierNewNormal ] );
+jsPDF.API.events.push( [ 'addFonts', addCourierNewBold ] );
+jsPDF.API.events.push( [ 'addFonts', addCourierNewItalic ] );
+jsPDF.API.events.push( [ 'addFonts', addCourierNewBoldItalic ] );
+
+jsPDF.API.events.push( [ 'addFonts', addTrebuchetMSNormal ] );
+jsPDF.API.events.push( [ 'addFonts', addTrebuchetMSBold ] );
+jsPDF.API.events.push( [ 'addFonts', addTrebuchetMSItalic ] );
+jsPDF.API.events.push( [ 'addFonts', addTrebuchetMSBoldItalic ] );
 
 const paymentsPDF = data => {
     const pdf = new jsPDF({
@@ -11,6 +43,11 @@ const paymentsPDF = data => {
     });
 
     pdf.setFont( 'courierNewNormal', 'normal' );
+    pdf.setFontSize( 16 );
+    pdf.setTextColor( 100, 100, 100 );
+    pdf.text( 20, 10, 'Κατάσταση οικονομικών, title somewhere here?!' );
+
+    pdf.setFont( 'trebuchetMSNormal', 'normal' );
     pdf.setFontSize( 16 );
     pdf.setTextColor( 100, 100, 100 );
     pdf.text( 20, 20, 'Κατάσταση οικονομικών, title somewhere here?!' );
@@ -36,6 +73,23 @@ const paymentsPDF = data => {
     pdf.setTextColor( 100, 100, 100 );
     pdf.text( 20, 70, 'Κατάσταση οικονομικών, title somewhere here?!' );
 
+
+    pdf.setFont( 'trebuchetMSBold', 'bold' );
+    pdf.setFontSize( 16 );
+    pdf.setTextColor( 100, 100, 100 );
+    pdf.text( 20, 80, 'Κατάσταση οικονομικών, title somewhere here?!' );
+
+    pdf.setFont( 'trebuchetMSItalic', 'italic' );
+    pdf.setFontSize( 16 );
+    pdf.setTextColor( 100, 100, 100 );
+    pdf.text( 20, 90, 'Κατάσταση οικονομικών, title somewhere here?!' );
+
+    pdf.setFont( 'trebuchetMSBoldItalic', 'bolditalic' );
+    pdf.setFontSize( 16 );
+    pdf.setTextColor( 100, 100, 100 );
+    pdf.text( 20, 100, 'Κατάσταση οικονομικών, title somewhere here?!' );
+
+    // https://micropyramid.com/blog/export-html-web-page-to-pdf-using-jspdf/
     // const cols = {
     //     serial: { width: 10, value: null },
     //     date: { width: 30, value: null },
