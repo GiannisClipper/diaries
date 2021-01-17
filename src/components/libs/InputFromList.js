64b7e762006c07ca76a0b0ce80fc97ffea639c68
giannisclipper <strunstrun@gmail.com>
2021-01-17 18:50:16 +0200

    Updating `InputFromList`.

--- src/components/libs/InputFromList.js
+++ src/components/libs/InputFromList.js
@@ -14,18 +14,18 @@ const InputIcon = styled.span`
 
 const InputList = styled.ul`
     position: fixed;
-    left: ${props => props.listBounds.left}px;
-    top: ${props => props.listBounds.top}px;
-    width: ${props => props.listBounds.width}px;
-    height: ${props => props.listBounds.height}px;
+    left: ${ props => props.listBounds.current.left }px;
+    top: ${ props => props.listBounds.current.top }px;
+    width: ${ props => props.listBounds.current.width }px;
+    height: ${ props => props.listBounds.current.height }px;
     overflow-y: auto;
     z-index: 999;
 
-    ${props => props.theme.InputList && props.theme.InputList };
+    ${ props => props.theme.InputList && props.theme.InputList };
 `;
 
 const InputItem = styled.li`
-    ${props => props.theme.InputItem && props.theme.InputItem };
+    ${ props => props.theme.InputItem && props.theme.InputItem };
 `;
 
 const simplify = value => value.toUpperCase();
@@ -49,7 +49,7 @@ function InputFromList( { className, allValues, value, onChange } ) {
 
     const listRef = useRef( null );
 
-    const [ listBounds, setListBounds ] = useState( {} );
+    const listBounds = useRef( {} );
 
     const listStatus = useRef( {} );
 
@@ -75,6 +75,7 @@ function InputFromList( { className, allValues, value, onChange } ) {
         listStatus.current = {};
         const index = 0;
         setMatch( { value, values: simplifyList( [ value ] ), index } );
+        onChange( { target: { value } } );  // this is the `InputFromList` attribute
     }
 
     const _onKeyDown = event => {
@@ -83,20 +84,21 @@ function InputFromList( { className, allValues, value, onChange } ) {
         let { value, values, index } = match;
 
         if ( match.values.length > 0 ) {
-            if ( event.keyCode === 38 ) {
+            if ( event.keyCode === 38 ) {  // up arrow
                 index = index > 0 ? index - 1 : match.values.length - 1;
                 setMatch( { value, values, index } );
 
-            } else if ( event.keyCode === 40 ) {
+            } else if ( event.keyCode === 40 ) {  // down arrow
                 index = index < match.values.length - 1 ? index + 1 : 0;
                 setMatch( { value, values, index } );
 
-            } else if ( event.keyCode === 13 ) {
+            } else if ( event.keyCode === 13 ) {  // enter
                 listStatus.current = {};
                 value = index >= 0 ? values[ index ].accurate : null;
                 values = value ? simplifyList( [ value ] ) : [];
                 index = value ? 0 : -1;
                 setMatch( { value, values, index } );
+                onChange( { target: { value } } );  // this is the `InputFromList` attribute
             }
         }
     }
@@ -114,53 +116,70 @@ function InputFromList( { className, allValues, value, onChange } ) {
             : values[ index ].accurate ;  // index >= 0, changed to valid value
 
         setMatch( { value, values, index } );
-        onChange( { target: { value } } );  // This is an `InputFromList` attribute
+        onChange( { target: { value } } );  // this is the `InputFromList` attribute
     }
 
     useEffect( () => {
         let { left, top, width, height } = inputRef.current.getBoundingClientRect();
         top = top + height;
         height = parseFloat( getComputedStyle( inputRef.current ).fontSize ) * 5;
-        setListBounds( { left, top, width, height } );
-    }, [] );
+
+        listBounds.current = { left, top, width, height } ;
+    } );
 
     useEffect( () => {
         if ( indexRef.current ) {
             let { top, height } = indexRef.current.getBoundingClientRect();
-            if ( top < listBounds.top ) {
-                listRef.current.scrollTop += top - listBounds.top;
-            } else if ( top + height > listBounds.top + listBounds.height ) {
-                listRef.current.scrollTop += ( top + height ) - ( listBounds.top + listBounds.height );
+
+            if ( top < listBounds.current.top ) {
+                listRef.current.scrollTop += top - listBounds.current.top;
+
+            } else if ( top + height > listBounds.current.top + listBounds.current.height ) {
+                listRef.current.scrollTop += ( top + height ) - ( listBounds.current.top + listBounds.current.height );
             }
         }
-    }, [ listBounds.top, listBounds.height ] );
+    } ); // listBounds.current.top, listBounds.current.height ] );
 
     let _key = -1;
 
     return (
         <Box>
-            <input ref={inputRef}
-                className={`InputFromList ${className}`}
-                value={match.value || ''}
-                onFocus={_onFocus}
-                onChange={_onChange}
-                onKeyDown={_onKeyDown}
-                onBlur={_onBlur}
+            <input ref={ inputRef }
+                className={ `InputFromList ${ className }` }
+                value={ match.value || '' }
+                onFocus={ _onFocus }
+                onChange={ _onChange }
+                onKeyDown={ _onKeyDown }
+                onBlur={ _onBlur }
             />
 
             <InputIcon onClick={() => listStatus.current.isOpen || _onKeyDown( { keyCode: 40 } ) }>
-                <FontAwesomeIcon icon={faCaretDown} />
+                <FontAwesomeIcon icon={ faCaretDown } />
             </InputIcon>
 
-            {listStatus.current.isOpen
-                ? <InputList ref={listRef} listBounds={listBounds}>
-                    {match.values.map( value =>
+            { listStatus.current.isOpen
+                ? 
+                <InputList ref={ listRef } listBounds={ listBounds }>
+                    { match.values.map( value =>
                         ++_key === match.index
-                            ? <InputItem index ref={indexRef} key={_key} onMouseDown={() => _onMouseDown( value.accurate )}> {value.accurate} </InputItem>
-                            : <InputItem key={_key} onMouseDown={() => _onMouseDown( value.accurate )}> {value.accurate} </InputItem>
-                    )}
+                            ? 
+                            <InputItem 
+                                index ref={ indexRef } 
+                                key={ _key } onMouseDown={ () => _onMouseDown( value.accurate ) }
+                            > 
+                                { value.accurate } 
+                            </InputItem>
+                            : 
+                            <InputItem 
+                                key={ _key } 
+                                onMouseDown={ () => _onMouseDown( value.accurate ) }
+                            > 
+                                { value.accurate } 
+                            </InputItem>
+                    ) }
                 </InputList>
-                : null
+                : 
+                null
             }
         </Box>
     )
